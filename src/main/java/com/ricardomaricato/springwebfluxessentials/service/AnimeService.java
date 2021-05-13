f687b09441a034701eb2ce942d2cdbfe1004880f
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-05-13 17:21:04 -0300

    -R2DBC Transactions

--- src/main/java/com/ricardomaricato/springwebfluxessentials/service/AnimeService.java
+++ src/main/java/com/ricardomaricato/springwebfluxessentials/service/AnimeService.java
@@ -2,13 +2,17 @@ package com.ricardomaricato.springwebfluxessentials.service;
 
 import com.ricardomaricato.springwebfluxessentials.domain.Anime;
 import com.ricardomaricato.springwebfluxessentials.repository.AnimeRepository;
+import io.netty.util.internal.StringUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -32,6 +36,18 @@ public class AnimeService {
 
     public Mono<Anime> save(Anime anime) {
         return animeRepository.save(anime);
+    }
+
+    @Transactional
+    public Flux<Anime> saveAll(List<Anime> animes) {
+        return animeRepository.saveAll(animes)
+                .doOnNext(this::throwResponseStatusExceptionWhenEmptyName);
+    }
+
+    private void throwResponseStatusExceptionWhenEmptyName(Anime anime){
+        if(StringUtil.isNullOrEmpty(anime.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Invalid Name");
+        }
     }
 
     public Mono<Void> update(Anime anime) {
--- src/test/java/com/ricardomaricato/springwebfluxessentials/SpringWebfluxEssentialsApplicationTests.java
+++ src/test/java/com/ricardomaricato/springwebfluxessentials/SpringWebfluxEssentialsApplicationTests.java
@@ -3,10 +3,10 @@ package com.ricardomaricato.springwebfluxessentials;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringWebfluxEssentialsApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
